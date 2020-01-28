0b343f06de11125f8b6dc2d08f935c000355ca17
costaarthur <costaarthur@hotmail.com>
2020-01-28 18:33:57 -0300

    fastfeet start model migrations

---
+++ src/app/models/FastfeetAdm.js
@@ -0,0 +1,32 @@
+import Sequelize, { Model } from 'sequelize';
+import bcrypt from 'bcryptjs';
+
+class Fastfeetadm extends Model {
+  static init(sequelize) {
+    super.init(
+      {
+        name: Sequelize.STRING,
+        email: Sequelize.STRING,
+        password: Sequelize.VIRTUAL,
+        password_hash: Sequelize.STRING,
+        provider: Sequelize.BOOLEAN,
+      },
+      {
+        sequelize,
+      }
+    );
+
+    // const password_hash = bcrypt.hashSync('123456', 8),
+    this.addHook('beforeSave', async adm => {
+      if (adm.password) {
+        adm.password_hash = await bcrypt.hash(adm.password, 8);
+      }
+    });
+  }
+
+  checkPass(password) {
+    return bcrypt.compare(password, this.password_hash);
+  }
+}
+
+export default Fastfeetadm;
