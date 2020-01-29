63c74a5792e86f3b60ff44717ae204c0a209bb0b
costaarthur <costaarthur@hotmail.com>
2020-01-29 19:40:42 -0300

    gg yup added

--- src/app/controllers/RecipientController.js
+++ src/app/controllers/RecipientController.js
@@ -1,8 +1,25 @@
-// import * as Yup from 'yup';
+import * as Yup from 'yup';
 import Recipient from '../models/Recipient';
 
 class RecipientController {
   async store(req, res) {
+    const schema = Yup.object().shape({
+      email: Yup.string()
+        .required()
+        .email(),
+      nome: Yup.string().required(),
+      rua: Yup.string().required(),
+      numero: Yup.number().required(),
+      complemento: Yup.string(),
+      estado: Yup.string().required(),
+      cidade: Yup.string().required(),
+      cep: Yup.number().required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
+
     const recipientExists = await Recipient.findOne({
       where: { email: req.body.email },
     });
@@ -37,6 +54,22 @@ class RecipientController {
   }
 
   async update(req, res) {
+    const schema = Yup.object().shape({
+      email: Yup.string()
+        .required()
+        .email(),
+      nome: Yup.string(),
+      rua: Yup.string(),
+      numero: Yup.number(),
+      complemento: Yup.string(),
+      estado: Yup.string(),
+      cidade: Yup.string(),
+      cep: Yup.number(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
     const { email } = req.body;
 
     const recipient = await Recipient.findOne({ where: { email } });
