d6349e3d5869e187d4ef6ca1466de72494c0443d
costaarthur <costaarthur@hotmail.com>
2020-02-05 14:51:13 -0300

    entfunctioncontroller finished

--- src/app/controllers/EntFunctionController.js
+++ src/app/controllers/EntFunctionController.js
@@ -1,4 +1,13 @@
 import * as Yup from 'yup';
+import {
+  startOfDay,
+  endOfDay,
+  parseISO,
+  isBefore,
+  isAfter,
+  addHours,
+} from 'date-fns';
+import { Op } from 'sequelize';
 import Encomenda from '../models/Encomenda';
 import Ent from '../models/Ent';
 
@@ -31,6 +40,8 @@ class EntFunctionController {
       id: Yup.number().required(),
       deliveryman_id: Yup.number().required(),
       start_date: Yup.date(),
+      end_date: Yup.date(),
+      signature_id: Yup.number(),
     });
 
     if (!(await schema.isValid(req.body))) {
@@ -42,7 +53,7 @@ class EntFunctionController {
     });
     // check encomenda exists
     if (!encomenda) {
-      return res.status(400).json({ error: 'Encomenda does not exists!' });
+      return res.status(400).json({ error: 'Delivery does not exists!' });
     }
     // check deliveryman exists
     const { deliveryman_id } = req.body;
@@ -56,11 +67,53 @@ class EntFunctionController {
 
     // check encomenda's owner
     if (encomenda.deliveryman_id !== deliveryman_id) {
-      return res.status(400).json({ error: 'This is not your encomenda!' });
+      return res.status(400).json({ error: 'This is not your delivery!' });
+    }
+    // check if start_date is past date
+    const { start_date } = req.body;
+    const hourStart = parseISO(start_date); // transforma a string em um objeto DATE.JS
+
+    if (isBefore(hourStart, new Date())) {
+      return res.status(400).json({ error: 'Past dates are not permitted' });
+    }
+    // check if start_date = 08:00 - 18:00h.
+    const jobBegin = addHours(startOfDay(hourStart), 8);
+    const jobEnd = addHours(startOfDay(hourStart), 18);
+
+    if (isBefore(hourStart, jobBegin)) {
+      return res
+        .status(400)
+        .json({ error: 'You cannot take the deliveries yet' });
+    }
+
+    if (isAfter(hourStart, jobEnd)) {
+      return res
+        .status(400)
+        .json({ error: 'You cannot take the deliveries anymore' });
+    }
+
+    // check <=5 retiradas/dia
+    const retiradasDia = await Encomenda.findAll({
+      where: {
+        deliveryman_id,
+        canceled_at: null,
+        start_date: {
+          [Op.between]: [startOfDay(hourStart), endOfDay(hourStart)],
+        },
+      },
+    });
+    if (retiradasDia.length > 4) {
+      return res
+        .status(400)
+        .json({ error: 'you cannot take more than 5 deliveries in one day' });
+    }
+    // add end_date
+    const { signature_id } = req.body;
+    if (signature_id) {
+      req.body.end_date = new Date();
     }
 
     const result = await encomenda.update(req.body);
-
     return res.json(result);
     /* return res.json({
       id,
